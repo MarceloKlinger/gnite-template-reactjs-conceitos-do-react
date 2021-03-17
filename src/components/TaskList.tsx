c4ed4c4347e3d1e99edfa3c736f06118cdb771f0
Marcelo Klinger <marcelo.klinger@hotmail.com>
2021-03-17 15:53:39 -0300

    Reset newTaskTitle

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -22,6 +22,7 @@ export function TaskList() {
       ...tasks,
       { id: Math.random(), title: newTaskTitle, isComplete: false },
     ]);
+    setNewTaskTitle(" ");
   }
 
   function handleToggleTaskCompletion(id: number) {
